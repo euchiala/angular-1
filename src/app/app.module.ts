3ccfc83703c7c39ead095ce6e7a08d9322f38335
Jerad Ahmed <jradahmed8@gmail.com>
2022-12-25 12:10:27 +0100

    commit form member

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MemberFormComponent } from './member-form/member-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MemberListComponent,
-    MemberFormComponent
+    MemberFormComponent,
+    MemberListComponent
   ],
   imports: [
+    ReactiveFormsModule,FormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
     MatIconModule,
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/member-form/member-form.component.ts
+++ src/app/member-form/member-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl,Validators } from '@angular/forms';
 
+import { Member } from 'src/model/Member';
 @Component({
   selector: 'app-member-form',
   templateUrl: './member-form.component.html',
   styleUrls: ['./member-form.component.css']
 })
-export class MemberFormComponent {
+export class MemberFormComponent implements OnInit {// 
+
+  form:any;
+  ngOnInit(): void {
+    this.initForm()
+  }
+  
+  initForm():void{
+    this.form = new FormGroup({
+      cin : new FormControl(null, [Validators.required]),
+      name : new FormControl(null, [Validators.required]),
+      cv : new FormControl(null, [Validators.required]),
+      type : new FormControl(null, [Validators.required])
+    })
+  }
+  onsub():void{
+    //recupruration et affichage des formulaire.
+    console.log(this.form.value)
+  }
 
 }
